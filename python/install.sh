c04ba38900348e8161043c9a32340ffe040796fb
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-01-24 22:37:31 +0900

    fix: requirements.txtのフィアル名変更

--- python/install.sh
+++ python/install.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR=$(cd $(dirname $0);pwd)
 $SCRIPT_DIR/venv.sh
 source $SCRIPT_DIR/venv/bin/activate
 
-pip install -r $SCRIPT_DIR/requirements.txt
-pip freeze > $SCRIPT_DIR/requirements_lock.txt
-pip wheel -r $SCRIPT_DIR/requirements_lock.txt -w $SCRIPT_DIR/pkgs
+pip install -r $SCRIPT_DIR/requirements.in.txt
+pip freeze > $SCRIPT_DIR/requirements.txt
+pip wheel -r $SCRIPT_DIR/requirements.txt -w $SCRIPT_DIR/pkgs
--- python/offline-install.sh
+++ python/offline-install.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$(cd $(dirname $0);pwd)
 $SCRIPT_DIR/venv.sh
 source $SCRIPT_DIR/venv/bin/activate
 
-pip install --no-deps --no-index -r $SCRIPT_DIR/requirements_lock.txt -f $SCRIPT_DIR/pkgs
+pip install --no-deps --no-index -r $SCRIPT_DIR/requirements.txt -f $SCRIPT_DIR/pkgs
