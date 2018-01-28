95564c698f6abc88fac7421b49bc88c39f063455
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-01-29 01:23:15 +0900

    fix: アーカイブファイル名に日付付与

--- make-archive.sh
+++ make-archive.sh
@@ -2,10 +2,9 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 BASE_DIR=$(dirname $SCRIPT_DIR)
 BASE_NAME=$(basename $SCRIPT_DIR)
-ARCHIVE=$BASE_NAME.tar.gz
 
-tar czf $ARCHIVE -C $BASE_DIR \
-    --exclude=$BASE_NAME/$ARCHIVE \
+tar czf $BASE_NAME-$(date +%Y%m%d).tar.gz -C $BASE_DIR \
+    --exclude=$BASE_NAME/$BASE_NAME-*.tar.gz \
     --exclude=$BASE_NAME/python/venv \
     --exclude=$BASE_NAME/*.retry \
     $BASE_NAME
