9761099bd611a64f480a9a803097a1b40ce5c20b
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-02-02 00:49:18 +0900

    fix: アーカイブスクリプト移動

--- python/make-archive.sh
+++ python/make-archive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
-BASE_DIR=$(dirname $SCRIPT_DIR)
-BASE_NAME=$(basename $SCRIPT_DIR)
+BASE_DIR=$(dirname $(dirname $SCRIPT_DIR))
+BASE_NAME=$(basename $(dirname $SCRIPT_DIR))
 
 tar czf $BASE_NAME-$(date +%Y%m%d).tar.gz -C $BASE_DIR \
     --exclude=$BASE_NAME/$BASE_NAME-*.tar.gz \
