3a1db05c8df554f18dbe46ad739135d9f4cf0b60
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-02-04 14:18:07 +0900

    clean: アーカイブスクリプトの変数名修正

--- python/make-archive.sh
+++ python/make-archive.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
-BASE_DIR=$(dirname $(dirname $SCRIPT_DIR))
-BASE_NAME=$(basename $(dirname $SCRIPT_DIR))
+BASE_DIR=$(dirname $SCRIPT_DIR)
+BASE_NAME=$(basename $BASE_DIR)
+PARENT_DIR=$(dirname $BASE_DIR)
 
-tar czf $BASE_NAME-$(date +%Y%m%d).tar.gz -C $BASE_DIR \
+tar czf $BASE_NAME-$(date +%Y%m%d).tar.gz -C $PARENT_DIR \
     --exclude=$BASE_NAME/$BASE_NAME-*.tar.gz \
     --exclude=$BASE_NAME/python/venv \
     --exclude=$BASE_NAME/*.retry \
